84af59362a5e1bdbca90177cf1507519ae01b193
Roman Shevchuk <roma.shev01@gmail.com>
2021-03-21 10:35:11 +0200

    JWT Auth

---
+++ backend/src/main/java/com/notes/backend/entities/LoginForm.java
@@ -0,0 +1,9 @@
+package com.notes.backend.entities;
+
+import lombok.Data;
+
+@Data
+public class LoginForm {
+    private String username;
+    private String password;
+}
---
+++ backend/src/main/java/com/notes/backend/services/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.notes.backend.services;
+
+import com.notes.backend.dao.UserRepository;
+import com.notes.backend.entities.User;
+import com.notes.backend.entities.UserDetailsImpl;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    final UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByLogin(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+        return UserDetailsImpl.build(user);
+    }
+}

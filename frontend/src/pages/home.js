975b6c9b54bd722a615901898f372909d413701e
M-Hasibur-Rahman <mhasibr10@gmail.com>
2021-04-03 21:40:18 +0200

    Register page and menu added

---
+++ frontend/src/pages/home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../components/Navbar/navbar.js';
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar />
+            <div id="body">
+            </div>
+        </div>
+    )
+}
+
+
+export default Home
---
+++ frontend/src/pages/signup.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navbar from '../components/Navbar/navbar.js';
+import RegisterForm from '../components/RegisterForm/registerform.js';
+
+const Signup = () => {
+    return (
+        <div>
+            <Navbar />
+            <div id="body">
+            <RegisterForm />
+            </div>
+        </div>
+    )
+}
+
+
+export default Signup

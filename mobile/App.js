efc8dbb1babc927dafc57d81609ae8f03abf2ef0
Kiryl Volkau <volkaukiryl@gmail.com>
2021-05-04 15:49:12 +0200

    Training for Illia
    Co-authored-by: Illia <IlliaMan@users.noreply.github.com>

--- mobile/App.js
+++ mobile/App.js
@@ -1,15 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState } from 'react';
+import { StyleSheet, Text, TouchableOpacity, View, TextInput, Button  } from 'react-native';
+import ComponentName from './ComponentName';
 
 export default function App() {
+
+  const [isPressed, setIdPressed] = useState(false);
+  const [text, setText] = useState('');
+
+  useEffect(() => {
+      fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(resp => resp.json())
+      .then(json => setText(JSON.stringify(json)));
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput placeholder="Login" maxLength={15}/>
+      <TextInput placeholder="Password" maxLength={15}/>
+      <View style={{backgroundColor:"yellow", width:15}}>
+        <Button title="Log in"/>
+      </View>
+      <Button title="Register"/>
+      <Text backgroundColor="green">Zdarova!</Text>
       <StatusBar style="auto" />
+      <TouchableOpacity onPress={() => setIdPressed(!isPressed)}> 
+      <Text>Press me</Text>
+      </TouchableOpacity>
+      {/* {
+        isPressed && <Text>{text ?? "null"}</Text>
+      } */}
+      {/* <Text>{text}</Text> */}
+      <Text>{isPressed.toString()}</Text>
+      <ComponentName isPressed={isPressed}/>
     </View>
-  );
-}
+  )};
 
 const styles = StyleSheet.create({
   container: {

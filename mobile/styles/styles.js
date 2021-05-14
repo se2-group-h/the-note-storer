4fe954d44d92f0be446ada15e12bc804c08b64d6
IlliaMan <illiamanzhela01@gmail.com>
2021-05-14 09:52:41 +0200

    add and change receipts

--- mobile/styles/styles.js
+++ mobile/styles/styles.js
@@ -1,42 +1,54 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fdfffc',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    button: {
-      alignItems: "center",
-      backgroundColor: "#011627",
-      padding: 10,
-      width: 175,
-      marginBottom: 15,
-    },
-    textColor: {
-      color: "#fdfffc",
-    },
-    textInput: {
-      height: 40,
-      borderColor: 'grey',
-      borderBottomWidth: StyleSheet.hairlineWidth,
-      marginBottom: 15,
-      width: 175,
-      textAlign: 'center'
-    },
-    button: {
-      alignItems: "center",
-      backgroundColor: "#011627",
-      padding: 10,
-      width: 175,
-      marginBottom: 15,
-    },
-    textColor: {
-      color: "#fdfffc",
-    }, 
-    welcome: {
-        fontSize: 25,
-        marginBottom: 25,
-    },    
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "#fdfffc",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#011627",
+    padding: 10,
+    width: 175,
+    marginBottom: 15,
+  },
+  textColor: {
+    color: "#fdfffc",
+  },
+  textInput: {
+    height: 40,
+    borderColor: "grey",
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    marginBottom: 15,
+    width: 175,
+    textAlign: "center",
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#011627",
+    padding: 10,
+    width: 175,
+    marginBottom: 15,
+  },
+  textColor: {
+    color: "#fdfffc",
+  },
+  welcome: {
+    fontSize: 25,
+    marginBottom: 25,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  users: {
+    fontSize: 30,
+    borderWidth: 1,
+    borderColor: "#cecece",
+    padding: 10,
+    marginBottom: 20,
+  },
+});
